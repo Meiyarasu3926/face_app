04f862853a0174a747580a9d496773c9460dbb34
Meiyarasu3926 <154041919+Meiyarasu3926@users.noreply.github.com>
2024-12-09 15:45:48 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,25 +11,3 @@ enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
 
-# Update system packages and install necessary dependencies
-apt-get update && apt-get install -y \
-    cmake \
-    pkg-config \
-    build-essential \
-    python3-dev \
-    libatlas-base-dev \
-    libboost-python-dev \
-    libboost-thread-dev \
-    libgtk2.0-dev \
-    libopenblas-dev \
-    liblapack-dev \
-    gfortran \
-    && rm -rf /var/lib/apt/lists/*
-
-# Upgrade pip
-python3 -m pip install --upgrade pip==24.3.1
-
-# Install Python dependencies from requirements.txt
-pip install -r requirements.txt
-
-echo "All dependencies installed successfully."
