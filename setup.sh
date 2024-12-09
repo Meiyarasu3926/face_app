001ff6e7f4ef4ff47d2674f29d543e16385dbde3
Meiyarasu3926 <154041919+Meiyarasu3926@users.noreply.github.com>
2024-12-09 16:55:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,17 +12,20 @@ enableCORS = false\n\
 
 
 
-# Update package lists and install dependencies
-apt-get update && apt-get install -y \
+apt-get install -y \
     cmake \
     pkg-config \
     build-essential \
     python3-dev \
     libatlas-base-dev \
+    libboost-all-dev \
+    libgomp1 \
     && rm -rf /var/lib/apt/lists/*
 
-
+# Upgrade pip to a specific version (24.3.1 in this case)
 python3 -m pip install --upgrade pip==24.3.1
+
+# Install the required Python packages from requirements.txt
 pip install -r requirements.txt
 
 echo "Dependencies installed successfully."
